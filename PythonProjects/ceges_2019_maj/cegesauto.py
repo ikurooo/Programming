f537c683421c3df1a2e11c03c271576cca85c03b
ikurooo <IvaneCankov@proton.me>
2023-01-10 14:38:12 +0100

    expanded

--- PythonProjects/ceges_2019_maj/cegesauto.py
+++ PythonProjects/ceges_2019_maj/cegesauto.py
@@ -1,77 +1,80 @@
-#1. Feladat: adatok beolvasasa
-autok = []
+def main():    #1. Feladat: adatok beolvasasa
+    autok = []
 
-with open('/home/ivan/Programming/PythonProjects/ceges_2018_maj/autok.txt') as file:
-    for line in file:
-        line = line.strip().split(" ")
-        auto = {
-            'nap' : int(line[0]),
-            'oraperc' : line[1],
-            'rendszam' : line[2],
-            'azonosito' : int(line[3]),
-            'km' : int(line[4]),
-            'ki' : True if line[5] == '0' else False
-        }
-        autok.append(auto)
+    with open('/home/ivan/Programming/PythonProjects/ceges_2018_maj/autok.txt') as file:
+        for line in file:
+            line = line.strip().split(" ")
+            auto = {
+                'nap' : int(line[0]),
+                'oraperc' : line[1],
+                'rendszam' : line[2],
+                'azonosito' : int(line[3]),
+                'km' : int(line[4]),
+                'ki' : True if line[5] == '0' else False
+            }
+            autok.append(auto)
 
-for auto in reversed(autok):
-    if auto['ki']:
-        print(f'2. Feladat\n{auto["nap"]}. nap rendszám: {auto["rendszam"]}')
-        break
+    for auto in reversed(autok):
+        if auto['ki']:
+            print(f'2. Feladat\n{auto["nap"]}. nap rendszám: {auto["rendszam"]}')
+            break
 
-print("3. feladat")
-try:
-    nap = int(input("Nap: "))
-except ValueError:
-    print("Helytelen ertek")
-    
-print(f'Forgalom a(z) {nap}. napon:')
+    print("3. feladat")
+    try:
+        nap = int(input("Nap: "))
+    except ValueError:
+        print("Helytelen ertek")
+        
+    print(f'Forgalom a(z) {nap}. napon:')
 
-for auto in autok:
-    if auto['nap'] == nap:
-        print(f'{auto["oraperc"]} {auto["rendszam"]} {auto["azonosito"]} {"ki" if auto["ki"] else "be"}')
-
-kint_van = set()
-for auto in autok:
-    if auto['ki']:
-        kint_van.add(auto['rendszam'])
-    else:
-        kint_van.remove(auto['rendszam'])
-
-print(f'4. feladat\nA hónap végén {len(kint_van)} autót nem hoztak vissza.')
-
-megtett = {}
-leghosszabb = 0
-
-for index, auto in enumerate(autok):
-    if not auto['ki']:
-        rendszam = auto['rendszam']
-        vissza = index - 1
-
-        while autok[vissza]["rendszam"] != rendszam and index != 0:
-            vissza -= 1
-
-        megtett_km = auto['km'] - autok[vissza]["km"]
-        if megtett_km > leghosszabb:
-            leghosszabb = megtett_km
-            szemely = auto['azonosito']
-
-        if megtett.get(rendszam):
-            megtett[rendszam] += megtett_km
-        else:
-            megtett[rendszam] = megtett_km
-
-for key, value in sorted(megtett.items()):
-    print(f'{key} {value} km')
-
-print(f'6. feladat\nLeghosszabb út: {leghosszabb} km, személy: {szemely}')
-
-rendszam = input("7. feladat\nRendszám: ")
-with open(rendszam + '_menetlevel.txt', 'w') as file:
     for auto in autok:
-        if auto["rendszam"] == rendszam and auto["ki"]:
-            print(f'{auto["azonosito"]}\t{auto["oraperc"]}\t{auto["km"]} km', file = file, end = '')
-        if auto["rendszam"] == rendszam and auto["ki"]:
-            print(f'{auto["oraperc"]}\t{auto["km"]} km', file = file)
+        if auto['nap'] == nap:
+            print(f'{auto["oraperc"]} {auto["rendszam"]} {auto["azonosito"]} {"ki" if auto["ki"] else "be"}')
 
-print('Menetlevél kész.')
+    kint_van = set()
+    for auto in autok:
+        if auto['ki']:
+            kint_van.add(auto['rendszam'])
+        else:
+            kint_van.remove(auto['rendszam'])
+
+    print(f'4. feladat\nA hónap végén {len(kint_van)} autót nem hoztak vissza.')
+
+    megtett = {}
+    leghosszabb = 0
+
+    for index, auto in enumerate(autok):
+        if not auto['ki']:
+            rendszam = auto['rendszam']
+            vissza = index - 1
+
+            while autok[vissza]["rendszam"] != rendszam and index != 0:
+                vissza -= 1
+
+            megtett_km = auto['km'] - autok[vissza]["km"]
+            if megtett_km > leghosszabb:
+                leghosszabb = megtett_km
+                szemely = auto['azonosito']
+
+            if megtett.get(rendszam):
+                megtett[rendszam] += megtett_km
+            else:
+                megtett[rendszam] = megtett_km
+
+    for key, value in sorted(megtett.items()):
+        print(f'{key} {value} km')
+
+    print(f'6. feladat\nLeghosszabb út: {leghosszabb} km, személy: {szemely}')
+
+    rendszam = input("7. feladat\nRendszám: ")
+    with open(rendszam + '_menetlevel.txt', 'w') as file:
+        for auto in autok:
+            if auto["rendszam"] == rendszam and auto["ki"]:
+                print(f'{auto["azonosito"]}\t{auto["oraperc"]}\t{auto["km"]} km', file = file, end = '')
+            if auto["rendszam"] == rendszam and auto["ki"]:
+                print(f'{auto["oraperc"]}\t{auto["km"]} km', file = file)
+
+    print('Menetlevél kész.')
+
+if __name__ == '__main__':
+    main()
--- PythonProjects/tarsalgo_2018_maj/tarsalgo.py
+++ PythonProjects/tarsalgo_2018_maj/tarsalgo.py
@@ -1,53 +1,77 @@
+def main():
+    #1. feladat; adatok beolvasasa
+    atkelesek = []
 
+    with open('/home/ivan/Programming/PythonProjects/tarsalgo_2018_maj/ajto.txt', 'r', encoding = "UTF-8") as file:
+        for line in file:
+            line = line.strip().split(' ')
+            atkeles = {
+                'ora' : int(line[0]),
+                'perc' : int(line[1]),
+                'azonosito' : int(line[2]),
+                'be' : True if line[3] == 'be' else False
+            }
 
-#1. feladat; adatok beolvasasa
-atkelesek = []
+            atkelesek.append(atkeles)
 
-with open('/home/ivan/Programming/PythonProjects/tarsalgo_2018_maj/ajto.txt', 'r', encoding = "UTF-8") as file:
-    for line in file:
-        line = line.strip().split(' ')
-        atkeles = {
-            'ora' : line[0],
-            'perc' : line[1],
-            'azonosito' : int(line[2]),
-            'be' : True if line[3] == 'be' else False
-        }
+    for atkeles in reversed(atkelesek):
+        if not atkeles['be']:
+            utolso_kilepo = atkeles['azonosito']
+            break
+        
+    print(f'2. feladat\nAz első belépő: {atkelesek[0]["azonosito"]}\nAz utolsó kilépő: {utolso_kilepo}')
 
-        atkelesek.append(atkeles)
+    #3. feladat; athaladasok meghatarozasa
+    athaladasok = {}
+    with open('/home/ivan/Programming/PythonProjects/tarsalgo_2018_maj/athaladas.txt', 'w') as file:
+        for atkeles in atkelesek:
+            azonosito = atkeles['azonosito']
+            if athaladasok.get(azonosito):
+                athaladasok[azonosito] += 1
+            else:
+                athaladasok[azonosito] = 1
 
-for atkeles in reversed(atkelesek):
-    if not atkeles['be']:
-        utolso_kilepo = atkeles['azonosito']
-        break
-    
-print(f'2. feladat\nAz első belépő: {atkelesek[0]["azonosito"]}\nAz utolsó kilépő: {utolso_kilepo}')
+        for key, azonosito in sorted(athaladasok.items()):
+            print(key, azonosito, file = file)
 
-#3. feladat; athaladasok meghatarozasa
-athaladasok = {}
-with open('/home/ivan/Programming/PythonProjects/tarsalgo_2018_maj/athaladas.txt', 'w') as file:
-    for atkeles in atkelesek:
-        azonosito = atkeles['azonosito']
-        if athaladasok.get(azonosito):
-            athaladasok[azonosito] += 1
+    legtobben = 0
+    bent_maradtak = set()
+    for index, atkeles in enumerate(atkelesek):
+        if atkeles['be']:
+            bent_maradtak.add(int(atkeles['azonosito']))
         else:
-            athaladasok[azonosito] = 1
+            bent_maradtak.remove(int(atkeles['azonosito']))
+        if len(bent_maradtak) > legtobben:
+            legtobben = len(bent_maradtak)
+            legtobben_index = index
 
-    for key, azonosito in sorted(athaladasok.items()):
-        print(key, azonosito, file = file)
+    print(f'4. feladat\nA végén a társalgóban voltak: ', end ='')
+    for azonosito in bent_maradtak:
+        print(azonosito, end = ' ')
 
-legtobben = 0
-bent_maradtak = set()
-for index, atkeles in enumerate(atkelesek):
-    if atkeles['be']:
-        bent_maradtak.add(int(atkeles['azonosito']))
-    else:
-        bent_maradtak.remove(int(atkeles['azonosito']))
-    if len(bent_maradtak) > legtobben:
-        legtobben = len(bent_maradtak)
-        legtobben_index = index
+    print(f'\n5. feladat\n{atkelesek[legtobben_index]["ora"]}:{atkelesek[legtobben_index]["perc"]}-kor voltak a legtöbben a társalgóban.')
 
-print(f'4. feladat\nA végén a társalgóban voltak: ', end ='')
-for azonosito in bent_maradtak:
-    print(azonosito, end = ' ')
+    try:
+        azonosito = int(input('6. feladat\nAdja meg a személy azonosítóját! '))
+    except ValueError:
+        print("Helytelen ertek")
 
-print(f'5. feladat\n{atkelesek[legtobben_index]["ora"]}:{atkelesek[legtobben_index]["perc"]}-kor voltak a legtöbben a társalgóban.')
+    print('7. feladat')
+    osszesen_bent_toltott_perc = 0
+    for atkeles in atkelesek:
+        if atkeles['azonosito'] == azonosito and atkeles['be']:
+            print(f'{atkeles["ora"]}:{atkeles["perc"]}', end = ' - ')
+            kezdo_perc = (atkeles["ora"] * 60) + atkeles["perc"]
+
+        elif atkeles['azonosito'] == azonosito and not atkeles['be']:
+            print(f'{atkeles["ora"]}:{atkeles["perc"]}')
+            vegzo_perc = (atkeles["ora"] * 60) + atkeles["perc"]
+            osszesen_bent_toltott_perc += vegzo_perc - kezdo_perc
+        
+    if azonosito in bent_maradtak:
+        osszesen_bent_toltott_perc += 900 - kezdo_perc
+
+    print(f'\n8. feladat\nA(z) {azonosito}. személy összesen {osszesen_bent_toltott_perc} percet volt bent, a megfigyelés\nvégén a társalgóban volt.')
+
+if __name__ == '__main__':
+    main()
