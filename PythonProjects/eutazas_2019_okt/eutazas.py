75412114fea84aaa94b06ce5a97bd7be0ff17855
ikurooo <IvaneCankov@proton.me>
2023-01-13 19:48:20 +0100

    added eutazas.py

---
+++ PythonProjects/eutazas_2019_okt/eutazas.py
@@ -0,0 +1,37 @@
+#1. feladat adatok beolvasasa
+utasok = []
+
+with open('/home/ivan/Programming/PythonProjects/eutazas_2019_okt/utasadat.txt', 'r', encoding = 'UTF-8') as file:
+    for line in file:
+        line = line.strip().split()
+        utas = {
+            'allomas' : int(line[0]),
+            'datum' : int(line[1][:8]),
+            'ido' : line[1][9:],
+            'azonosito' : int(line[2]),
+            'tipus' : line[3],
+            'ervenyes' : int(line[4])
+        }
+        utasok.append(utas)
+
+print(f'2. feladat\nA buszra {len(utasok)} utas akart felszállni.')
+
+nem_szallhattak_fel = 0
+egy_allomas = 0
+legtobben = 0
+megallo = 0
+
+for index, utas in enumerate(utasok):
+    if utas['ervenyes'] == 0 or utas['datum'] > utas['ervenyes'] and utas['tipus'] != 'JGY':
+        nem_szallhattak_fel += 1
+    if utas['allomas'] == utasok[index - 1]['allomas']:
+        egy_allomas += 1
+    else:
+        egy_allomas += 1
+        if egy_allomas > legtobben:
+            legtobben = egy_allomas
+            megallo = utasok[index - 1]['allomas']
+        egy_allomas = 0
+
+print(f'3. feladat\nA buszra {nem_szallhattak_fel} utas nem szállhatott fel.')
+print(f'4. feladat\nA legtöbb utas ({legtobben} fő) a {megallo}. megállóban próbált felszállni.')
