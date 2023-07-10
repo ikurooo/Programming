9cbf3443f8a036d6f2538abf32b6347594b46a3a
ikurooo <IvaneCankov@proton.me>
2023-07-10 13:30:12 +0100

    Delete JavaProjects/Mobile Tree directory

--- JavaProjects/Mobile Tree/probetest2_Mi/probetest2_Mi/src/Countable.java
+++
@@ -1,8 +0,0 @@
-// An object with countable elements.
-// Please, do not change this interface definition!
-//
-public interface Countable {
-
-    // Returns the number of elements in 'this'.
-    int count();
-}
