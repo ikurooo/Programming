aea0ef499350e12287facbb36d34223e9bc11bde
ikurooo <IvaneCankov@proton.me>
2023-07-10 13:29:31 +0100

    Delete JavaProjects/probetest2_Mo directory

--- JavaProjects/probetest2_Mo/sol/UnbalancedException.java
+++
@@ -1,8 +0,0 @@
-// An Exception thrown by 'BalancedStick'
-//
-public class UnbalancedException extends Exception {
-
-    public UnbalancedException (String message){
-        super(message);
-    }
-}
--- JavaProjects/probetest2_Mo/src/UnbalancedException.java
+++
@@ -1,8 +0,0 @@
-// An Exception thrown by 'BalancedStick'
-//
-public class UnbalancedException extends Exception {
-
-    public UnbalancedException(String s) {
-        super(s);
-    }
-}
