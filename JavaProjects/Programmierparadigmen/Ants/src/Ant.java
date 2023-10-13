68ab69479d3fb71aec4c1b7335de13ee918c2601
ikurooo <IvaneCankov@proton.me>
2023-10-13 15:04:19 +0100

    fixed some issues

--- JavaProjects/Programmierparadigmen/Ants/src/Ant.java
+++ JavaProjects/Programmierparadigmen/Ants/src/Ant.java
@@ -6,10 +6,10 @@ public class Ant {
     private State state;
     private int[] position; // position[0]=width, position[1]=height
 
-    public Ant(int[] posAntHill) {						        // Ants now take in a height, width argument so that they
-        this.state = State.ERKUNDUNG;						    // can be spawned dead centre
-        this.position = new int[]{posAntHill[0], posAntHill[1]};   // spawns ants in centre of AntHill
-        this.random = new Random();			 				    // TODO: summons a new random every time gotta fix this
+    public Ant(int[] posAntHill) {						        	// Ants now take in a height, width argument so that they
+        this.state = State.ERKUNDUNG;						    	// can be spawned dead centre
+        this.position = new int[]{posAntHill[0], posAntHill[1]};    // spawns ants in centre of AntHill
+        this.random = new Random();			 				    	// TODO: summons a new random every time gotta fix this
     }
 
     public int[] getPosition() {
@@ -25,9 +25,7 @@ public class Ant {
     }
 
     public void randomMove(Field field) {
-        boolean moved = false;
-
-        int[][] directions = {
+        int[][] directions = {  				//These are the initial moves that an ant can make
                 {0, 1},    // North
                 {1, 1},    // Northeast
                 {1, 0},    // East
@@ -41,24 +39,27 @@ public class Ant {
         int num = -1;
         if (lastDirection == null) {
             num = random.nextInt(8);
+            lastDirection = new int[2];
         } else {
             num = random.nextInt(5);
-
-            //there is literally a bug here i can't
-            directions = new int[][]{								//lastMove 1,1
-                    {lastDirection[0], (lastDirection[1] + 1) % 2},		// 1,0
-                    {-lastDirection[0], (lastDirection[1] - 1) % 2},		// 1,0
-                    {lastDirection[0], lastDirection[1]},				// 1,1
-                    {(lastDirection[0] + 1) % 2, lastDirection[1]},		// 0,1
-                    {(lastDirection[0] - 1) % 2, -lastDirection[1]}		// 0,1
+            directions = new int[][]{										//lastMove 1,1
+                    {lastDirection[0], (lastDirection[1] + 1) % 2},			// 1,0
+                    {-lastDirection[0], (lastDirection[1] - 1) % 2},		// -1,0
+                    {lastDirection[0], lastDirection[1]},					// 1,1
+                    {(lastDirection[0] + 1) % 2, lastDirection[1]},			// 0,1
+                    {(lastDirection[0] - 1) % 2, -lastDirection[1]}			// 0,-1
+                    // Used the modulus operator so something like 2,2 doesn't happen
             };
         }
-        //Probably the dumbest way to go about implementing this, but it is what it is
-
+        // Loads the new position into temporary variables
         int newX = position[0] + directions[num][0];
         int newY = position[1] + directions[num][1];
-        lastDirection = new int[]{directions[num][0], directions[num][1]};
 
+        // Saves the last direction moved
+        lastDirection[0] = directions[num][0];
+        lastDirection[1] = directions[num][1];
+
+        // Moves the ant modulus operator used so the array behaves like a torus
         position[0] = newX % field.getFieldDimensions()[1];
         position[1] = newY % field.getFieldDimensions()[0];
     }
