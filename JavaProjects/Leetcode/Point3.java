2ef2b3566344c88a77af05aa74a8b4ffdc38b3f5
ikurooo <ivanecankov@proton.me>
2025-07-20 15:23:57 +0200

    walls and gates 286 solved with iterative bfs

---
+++ JavaProjects/Leetcode/Point3.java
@@ -0,0 +1,35 @@
+class Point3<X, Y, Z> {
+    private X x;
+    private Y y;
+    private Z z;
+
+    public Point3(X x, Y y, Z z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public X getX() {
+        return x;
+    }
+
+    public Y getY() {
+        return y;
+    }
+
+    public Z getZ() {
+        return z;
+    }
+
+    public void setX(X x) {
+        this.x = x;
+    }
+
+    public void setY(Y y) {
+        this.y = y;
+    }
+
+    public void setZ(Z z) {
+        this.z = z;
+    }
+}
